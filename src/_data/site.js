56a5114ced2dc379624179ad6f08b2d9de14374b
Ryan Roberts <ryan@yep.digital>
2024-03-20 12:26:42 +0000

    Remove unused deets

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,5 @@ module.exports = {
   title: 'Aberdeen website design and development with Yep Digital',
   description: 'Yep Digital creates websites for small to medium size businesses, third-sector organisations and individuals throughout Scotland',
   keywords: ['web design', 'website design', 'aberdeen web design', 'web development', 'web development'],
-  url: 'https://yep.digital',
-  theme: '#18181b',
-  darkTheme: '#19121e',
+  url: 'https://yep.digital'
 };
