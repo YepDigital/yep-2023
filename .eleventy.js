463024ae5d6f44fc7cfcefaea7c3abca9c8c1b25
Ryan Roberts <ryan@yep.digital>
2024-07-02 15:43:55 +0100

    Cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -18,21 +18,7 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
-
   eleventyConfig.addLayoutAlias('default', 'default.njk');
-
-  // Add shortlink redirects
-  // https://www.raymondcamden.com/2021/06/22/dynamic-short-urls-with-eleventy
-  // eleventyConfig.addCollection("goPages", collectionApi => {
-  //   return collectionApi.getAll().filter(p => {
-  //     if(p.data.go) return true;
-  //     return false;
-  //   });
-  // });
 
   return {
     dir: {
