6add06d6f0b3a17bf62d34cb84957693811e8b3d
Ryan Roberts <ryan@yep.digital>
2024-07-28 14:12:25 +0100

    Fix ampersand in title

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Freelance Website Design & Development • Yep Digital',
+  title: 'Freelance Website Design &amp; Development • Yep Digital',
   description: 'Ryan is a freelance web developer with 20 years experience working in the UK design industry. Ryan specialises in results-driven websites for businesses of all sizes.',
 	keywords: ['web design', 'website design', 'aberdeen web design', 'web development', 'freelance web developer', 'Scotland web designer', 'results-driven websites', 'business website development', 'custom website creation', 'North-East Scotland web services'],
   url: 'https://yep.digital'
