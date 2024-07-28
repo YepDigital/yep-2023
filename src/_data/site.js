d09d8119ca55801aa75e8f9ba467f29d642711e1
Ryan Roberts <ryan@yep.digital>
2024-07-28 14:15:14 +0100

    Rewrit title and desc

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Freelance Website Design &amp; Development • Yep Digital',
-  description: 'Ryan is a freelance web developer with 20 years experience working in the UK design industry. Ryan specialises in results-driven websites for businesses of all sizes.',
+  title: 'Experienced Freelance Web Developer in North-East Scotland | Yep Digital',
+  description: 'Ryan is a freelance web developer with 20 years of experience, specializsing in creating effective, visually appealing websites for businesses of all sizes.',
 	keywords: ['web design', 'website design', 'aberdeen web design', 'web development', 'freelance web developer', 'Scotland web designer', 'results-driven websites', 'business website development', 'custom website creation', 'North-East Scotland web services'],
   url: 'https://yep.digital'
 };
